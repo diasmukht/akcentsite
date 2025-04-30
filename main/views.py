edcd264721ab76a184bd1885b37802dd0ab98589
diasmukht <diaspias05@gmail.com>
2025-04-30 21:19:13 +0600

    Added CSRF for login and singup

--- main/views.py
+++ main/views.py
@@ -1,8 +1,12 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import CustomUserCreationForm
+from django.contrib import messages
 def index(request):
     return render(request, 'index.html')
 
 
-from django.shortcuts import render
+
 
 
 def about(request):
@@ -21,14 +25,6 @@ def instructor(request):
     return render(request, 'instructor.html')
 
 
-def login_view(request):
-    return render(request, 'login.html')
-
-
-def signup(request):
-    return render(request, 'signup.html')
-
-
 def team(request):
     return render(request, 'team.html')
 
@@ -40,11 +36,59 @@ def testimonial(request):
 def single(request):
     return render(request, 'single.html')
 
+# ✅ Регистрация
+def signup_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
-from django.http import HttpResponse
-from django.core.management import call_command
+
+# ✅ Вход
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Қате email немесе пароль')
+    return render(request, 'login.html')
 
 
-def migrate_now(request):
-    call_command("migrate")
-    return HttpResponse("✅ Миграции выполнены на сервере.")
+# ✅ Выход
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.http import HttpResponse
+# from django.core.management import call_command
+
+
+# def migrate_now(request):
+#     call_command("migrate")
+#     return HttpResponse("✅ Миграции выполнены на сервере.")
